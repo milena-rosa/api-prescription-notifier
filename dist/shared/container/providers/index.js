93e739e8f51ae5678795060b33639ae8fd7409fc
Milena Boselli Rosa <milena.rosa@gmail.com>
2021-11-21 15:43:24 -0300

    removed some files.

--- dist/shared/container/providers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayjsDateProvider = require("./DateProvider/implementations/DayjsDateProvider");
-
-_tsyringe.container.registerSingleton('DateProvider', _DayjsDateProvider.DayjsDateProvider);
