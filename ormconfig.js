ece842428be46719c3ff84224465f68d9e759b4c
Milena Boselli Rosa <milena.rosa@gmail.com>
2021-11-29 19:38:20 -0300

    removed connection name

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,5 @@
 console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL)
 module.exports = {
-  name: 'default',
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
