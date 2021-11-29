8e40907deeb05ad659e0bc1a66b3821c8b21be39
Milena Boselli Rosa <milena.rosa@gmail.com>
2021-11-29 17:19:28 -0300

    changed heroku postbuild script. testing.

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL)
+module.exports = {
+  name: 'default',
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  logging: true,
+  migrations: ['dist/shared/infra/typeorm/migrations /*.ts'],
+  entities: ['dist/modules/**/entities/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations'
+  }
+}
