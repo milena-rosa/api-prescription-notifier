14143c2982f594d8f592083a658e6c44baab9091
Milena Boselli Rosa <milena.rosa@gmail.com>
2021-11-29 16:27:36 -0300

    changed typeorm connection.

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -9,6 +9,8 @@ getConnectionOptions().then((options) => {
   newOptions.host = 'database'
 
   createConnection({
-    ...options
+    // ...options,
+    type: 'postgres',
+    url: 'postgres://flaqkxfhooanxa:76f58e01d4fd4360046898a4ce2f21e157cb7ea142528054bfb736b35b99548f@ec2-18-210-159-154.compute-1.amazonaws.com:5432/d7567ps5jrcf9k'
   })
 })
